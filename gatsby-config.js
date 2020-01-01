f54b5b39953d9f67af8c142a539145f08cc05e81
Yvonne Nkr <yvonnkr86@gmail.com>
2020-01-01 04:15:03 +0000

    added env vars and contenful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,5 +21,12 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-transition-link`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
   ],
 };
